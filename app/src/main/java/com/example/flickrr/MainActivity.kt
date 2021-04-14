fa013b463d884d7612da25bf01ca6951dd139159
Akshat0410 <anupamp74@gmail.com>
2021-04-15 03:09:53 +0530

    Handled Click Listeners for Share Download and Image App is fine and working

--- app/src/main/java/com/example/flickrr/MainActivity.kt
+++ app/src/main/java/com/example/flickrr/MainActivity.kt
@@ -1,20 +1,13 @@
 package com.example.flickrr
 
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.GridLayout
-import android.widget.TextView
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.flickrr.Model.Photo
-import com.example.flickrr.Model.Photos
 import com.example.flickrr.repository.Repository
-import org.w3c.dom.Text
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,6 +18,7 @@ class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var dataList:List<Photo>
@@ -33,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         val viewModelFactory=MainViewModelFactory(repository)
         viewModel=ViewModelProvider(this,viewModelFactory).get(MainViewModel::class.java)
         viewModel.getPost()
-        viewModel.MyResponse.observe(this, Observer { response ->
+        viewModel.MyResponse.observe(this, { response ->
             dataList= response.photos.photo
             recyclerView.layoutManager=GridLayoutManager(applicationContext,2)
             adapter= RecyclerViewAdapter(this, dataList)
@@ -41,5 +35,8 @@ class MainActivity : AppCompatActivity() {
         })
 
 
+
     }
 }
+
+
